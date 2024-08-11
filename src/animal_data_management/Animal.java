1785aa37c5e0082aa960312dccb3b0f9f5d8af0a
s1thu <s1thu.w1n.5601@gmail.com>
2024-08-11 12:47:00 +0630

    [animal_data_management] Refractor

--- src/animal_data_management/Animal.java
+++ src/animal_data_management/Animal.java
@@ -5,10 +5,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class Animal {
-
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String name;
     int age;
     String species;
@@ -18,7 +19,6 @@ public class Animal {
     }
 
     public void inputData() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter animal name:");
         this.name = br.readLine();
         System.out.print("Enter animal age:");
@@ -31,5 +31,38 @@ public class Animal {
         System.out.println("Enter animal name:" + name);
         System.out.println("Enter animal age:" + age);
         System.out.println("Enter animal species:" + species);
+    }
+
+    private static boolean isValidResponse(String response, String[] validResponses) {
+        for (String validResponse : validResponses) {
+            if (response.equals(validResponse)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void displayAnimalInfo(List<Animal> animals) {
+        System.out.println("Total number of animals:" + animals.size());
+        System.out.println();
+        for (Animal animal : animals) {
+            animal.display();
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String loopCondition = "";
+        String[] validResponses = {"yes", "y"};
+        List<Animal> animals = new ArrayList<>();
+        do {
+            Animal animal = new Animal();
+            animal.inputData();
+            animals.add(animal);
+            System.out.println("Do you want to enter another animal?(yes/no)");
+            loopCondition = br.readLine();
+        } while (isValidResponse(loopCondition.toLowerCase(), validResponses));
+
+        displayAnimalInfo(animals);
     }
 }
--- src/animal_data_management/Main.java
+++ src/animal_data_management/Main.java
@@ -10,35 +10,13 @@ import java.util.Set;
 
 public class Main
 {
-    private static boolean isValidResponse(String response, String[] validResponses) {
-        for (String validResponse : validResponses) {
-            if (response.equals(validResponse)) {
-                return true;
-            }
-        }
-        return false;
-    }
+
 
     public static void main(String[] args) throws IOException {
 
-        String loopCondition = "";
-        String[] validResponses = {"yes", "y"};
-        List<Animal> animals = new ArrayList<>();
-        do {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            Animal animal = new Animal();
-            animal.inputData();
-            animals.add(animal);
-            System.out.println("Do you want to enter another animal?(yes/no)");
-            loopCondition = br.readLine();
-        } while (isValidResponse(loopCondition.toLowerCase(), validResponses));
 
-        System.out.println("Total number of animals:" + animals.size());
-        System.out.println();
-        for (Animal animal : animals) {
-            animal.display();
-            System.out.println();
-        }
+
+
 
     }
 
